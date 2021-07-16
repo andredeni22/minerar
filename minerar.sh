fccecc1e048ea678a56278f5a1aed2f621677fb2
andredeni22 <87532233+andredeni22@users.noreply.github.com>
2021-07-16 10:27:31 -0300

    Update minerar.sh

--- minerar.sh
+++ minerar.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt-get update -y; apt-get upgrade -y
 apt-get install htop
+apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev libncurses5-dev pkg-config yasm
 git clone https://github.com/tpruvot/cpuminer-multi
 cd cpuminer-multi
 ./build.sh
